d9e6fe10a513ac4b3ccbfa71bcaa37237b2ff71e
dmitry <boldin.dmitry@gmail.com>
2020-04-16 18:17:38 +0300

    fixed abs

--- exercises_abs.py
+++ exercises_abs.py
@@ -9,7 +9,7 @@ class ExerciseAbstract(abc.ABC):
         """
         list of strings
         """
-        return []
+        pass
 
     def description(self):
         return random.choice(self._description)
@@ -20,7 +20,7 @@ class ExerciseAbstract(abc.ABC):
         """
         min and max
         """
-        return [0, 0]
+        pass
 
     def pulse(self):
         return random.randint(self._pulse[0], self._pulse[1])
@@ -31,7 +31,7 @@ class ExerciseAbstract(abc.ABC):
         """
         list of strings
         """
-        return [""]
+        pass
 
     def state_of_health(self):
         return random.choice(self._state_of_health)
