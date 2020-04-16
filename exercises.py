3f2fdac6d3356ebb3ba7484f7b12c131879c16e6
dmitry <boldin.dmitry@gmail.com>
2020-04-16 21:12:22 +0300

    all exercises field inputs for author info added

--- exercises.py
+++ exercises.py
@@ -9,7 +9,6 @@ class Rod20(ExerciseAbstract):
     _state_of_health = ["Хорошее",
                         "Нормальное",
                         "В норме",
-                        "В порядке",
                         "Легкая одышка"
                         "Немного устал",
                         "Немного болят руки",
@@ -21,4 +20,284 @@ class Rod20(ExerciseAbstract):
 
     def description(self):
         repeat = ["3x25", "4x25", "3x20", "4x20", "5x20"]
-        return self._description + random.choice(repeat)
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class LungesDB(ExerciseAbstract):
+    _description = "Выпады с гантелями"
+    _pulse = [145, 170]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят ноги",
+                        "Одышка",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x15 5кг",
+                  "4x15 5кг",
+                  "3x20 5кг",
+                  "4x20 5кг",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class PullUp(ExerciseAbstract):
+    _description = "Подтягивания"
+    _pulse = [145, 160]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Болят руки",
+                        "Болят мышцы спины",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["2x10 ",
+                  "4x15 ",
+                  "3x15 ",
+                  "4x12 ",
+                  "3x10 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class PushUp(ExerciseAbstract):
+    _description = "Отжимания"
+    _pulse = [125, 150]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Болят руки",
+                        "Одышка",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x20 ",
+                  "4x15 ",
+                  "3x15 ",
+                  "4x20 ",
+                  "2x30 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class ArmCurl10(ExerciseAbstract):
+    _description = "Сгибание рук с гантелями 10кг"
+    _pulse = [125, 150]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят руки",
+                        "Болят руки",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x25 ",
+                  "4x25 ",
+                  "3x20 ",
+                  "4x20 ",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class ArmExtension5(ExerciseAbstract):
+    _description = "Разгибание руки из-за головы с гантелей 5кг"
+    _pulse = [135, 150]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят руки",
+                        "Болят руки",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x25 ",
+                  "4x25 ",
+                  "3x20 ",
+                  "4x20 ",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class ArmAbduction5(ExerciseAbstract):
+    _description = "Отведение рук с гантелями 5кг"
+    _pulse = [135, 155]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят руки",
+                        "Немного болят плечи",
+                        "Болят плечи",
+                        "Болят руки",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x25 ",
+                  "4x25 ",
+                  "3x20 ",
+                  "4x20 ",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class ArmCurl20(ExerciseAbstract):
+    _description = "Сгибание рук со штангой 20кг"
+    _pulse = [135, 170]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят руки",
+                        "Болят плечи",
+                        "Болят руки",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x25 ",
+                  "4x25 ",
+                  "3x20 ",
+                  "4x20 ",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class RodTraction20(ExerciseAbstract):
+    _description = "Тяга штанги 20кг"
+    _pulse = [135, 170]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят руки",
+                        "Болят плечи",
+                        "Болит спина",
+                        "Немного болит спина",
+                        "Болят руки",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x20 ",
+                  "4x20 ",
+                  "3x15 ",
+                  "4x12",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class BackPushUp(ExerciseAbstract):
+    _description = "Обратные отжимания"
+    _pulse = [125, 150]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Немного болят руки",
+                        "Болят плечи",
+                        "Болит спина",
+                        "Немного болит спина",
+                        "Болят руки",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x20 ",
+                  "4x20 ",
+                  "3x15 ",
+                  "4x12",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class Burpee(ExerciseAbstract):
+    _description = "Берпи"
+    _pulse = [125, 180]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "В норме",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x20 ",
+                  "4x20 ",
+                  "3x15 ",
+                  "4x12",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class Twisting(ExerciseAbstract):
+    _description = "Скручивания"
+    _pulse = [125, 180]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "Болит пресс",
+                        "Немного болит пресс",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x25 ",
+                  "4x20 ",
+                  "3x30 ",
+                  "4x25",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
+
+
+class BackTwisting(ExerciseAbstract):
+    _description = "Скручивания"
+    _pulse = [125, 180]
+    _state_of_health = ["Хорошее",
+                        "Нормальное",
+                        "Болит пресс",
+                        "Немного болит пресс",
+                        "Легкая одышка"
+                        "Немного устал",
+                        "Усталость",
+                        ]
+
+    def description(self):
+        repeat = ["3x25 ",
+                  "4x20 ",
+                  "3x30 ",
+                  "4x25",
+                  "5x20 ",
+                  ]
+        return self._description + " " + random.choice(repeat) + "\n"
--- main.py
+++ main.py
@@ -13,10 +13,43 @@ def random_day(x):
     return random.choice([x - one_day, x, x + one_day])
 
 
-if __name__ == '__main__':
-    # start_input = input("Введите дату в формате: %d-%m-%Y")
-    start = datetime.datetime.strptime("21-06-2014", "%d-%m-%Y")
-    end = datetime.datetime.strptime("07-07-2014", "%d-%m-%Y")
+if __name__ == "__main__":
+    document = Document()
+    document.add_heading("Дневник самоподготовки", 0)
+
+    name = input("ФИО:\n")
+    date_of_birth = input("Дата рождения:\n")
+    group = input("Группа:\n")
+    weight = input("Вес:\n")
+    height = input("Рост:\n")
+
+    table = document.add_table(rows=1, cols=2)
+
+    row = table.add_row().cells
+    row[0].text = "ФИО"
+    row[1].text = name
+
+    row = table.add_row().cells
+    row[0].text = "Дата рождения"
+    row[1].text = date_of_birth
+
+    row = table.add_row().cells
+    row[0].text = "Группа"
+    row[1].text = group
+
+    row = table.add_row().cells
+    row[0].text = "Вес"
+    row[1].text = weight
+
+    row = table.add_row().cells
+    row[0].text = "Рост"
+    row[1].text = height
+
+    start_str = input("дата начала занятий, формат: %d.%m.%Y:\n")
+    end_str = input("дата конца занятий, формат: %d.%m.%Y:\n")
+
+    start = datetime.datetime.strptime(start_str, "%d.%m.%Y")
+    end = datetime.datetime.strptime(end_str, "%d.%m.%Y")
 
     frequency = 4
 
@@ -31,37 +64,36 @@ if __name__ == '__main__':
         if inspect.isclass(obj) and obj != exercises_abs.ExerciseAbstract:
             all_exercises.append(obj())
 
-    document = Document()
     table = document.add_table(rows=1, cols=5)
+    table.style = "Table Grid"
 
     hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'День недели, число, месяц, год, время занятия'
-    hdr_cells[1].text = 'Содержания физкультурного занятия'
-    hdr_cells[2].text = 'Пульс'
-    hdr_cells[3].text = 'Самочувствие'
-    hdr_cells[4].text = 'Желание заниматься'
+    hdr_cells[0].text = "День недели, число, месяц, год, время занятия"
+    hdr_cells[1].text = "Содержания физкультурного занятия"
+    hdr_cells[2].text = "Пульс"
+    hdr_cells[3].text = "Самочувствие"
+    hdr_cells[4].text = "Желание заниматься"
 
     for date in date_generated:
         string_date = ", ".join([weekdays[date.weekday()], date.strftime("%d.%m.%Y"), random.choice(day_time)])
 
         random.shuffle(all_exercises)
 
-        exercises_count = 1
-        description, pulse, state_of_health = "", [], ""
+        exercises_count = 4
+        description, pulse, state_of_health = "", "", ""
 
         for i in range(exercises_count):
-            description += (all_exercises[i].description() + " ")
-            pulse.append(all_exercises[i].pulse())
-            state_of_health += (all_exercises[i].state_of_health() + " ")
-
-        pulse_int = int(sum(pulse) / len(pulse))
+            description += all_exercises[i].description() + "\n"
+            pulse += str(all_exercises[i].pulse()) + "\n"
+            state_of_health += all_exercises[i].state_of_health() + "\n"
 
         row_cells = table.add_row().cells
 
         row_cells[0].text = string_date
         row_cells[1].text = description
-        row_cells[2].text = str(pulse_int)
+        row_cells[2].text = pulse
         row_cells[3].text = state_of_health
         row_cells[4].text = "+"
 
-    document.save('demo.docx')
+    document_name = input("название документа:\n")
+    document.save(document_name + ".docx")
